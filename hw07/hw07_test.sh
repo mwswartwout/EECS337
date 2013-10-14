9647c0270ac7c70800412d97a74998b5ada4226b
Matthew Swartwout <mwswartwout@gmail.com>
2013-10-14 10:58:58 -0400

    Added hw07 directory and populated with the correct files

---
+++ hw07/hw07_test.sh
@@ -0,0 +1,15 @@
+#
+#	hw07_test.sh
+#
+make clean
+make
+./calc < math10.txt
+./calc < math11.txt
+./calc < math12.txt
+./calc < math13.txt
+./calc < math14.txt
+./calc < math15.txt
+./calc < math16.txt
+./calc < math17.txt
+./calc < math18.txt
+./calc < math19.txt
